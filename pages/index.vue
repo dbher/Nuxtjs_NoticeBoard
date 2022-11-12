6c7acf433f896d539e3ea485a47a1a1ac18176ba
zeldabean <irenelemon00@naver.com>
2022-11-12 22:40:47 +0900

    adding writing form and list

--- pages/index.vue
+++ pages/index.vue
@@ -1,48 +1,98 @@
 <template>
-  <div class="container">
-    <div class ="header">
-      123345
-      <!-- <NuxtLink to="/submenu/write" id = "sub_menu">글쓰기</NuxtLink>
-      <button @click="write">새 글 쓰기</button> -->
+  <!-- <Nuxt /> -->
+  <div class="defalut_components">
+    <div class="header">
+      <NuxtLink to="/" id = "main_title">게시판</NuxtLink>
+      <NuxtLink to="/submenu/write" id = "sub_menu">글쓰기</NuxtLink>
     </div>
-    <div class = "contents">
-      <h2>목록ㄴㅇㄹㄴㅇㄹ</h2>
-      <NuxtLink to="/write" class="linkBtn">글쓰기</NuxtLink>
-      <NuxtLink to="/list" class="linkBtn">리스트</NuxtLink>
-      <!-- <NuxtLink to="/test" class="linkBtn">테스트</NuxtLink>  -->
-      <!-- <Nuxt /> -->
+    <div class = "content">
+      <h2>글 목록</h2>
+      <table class = "table_list">
+        <thead>
+          <tr :key="index" v-for="(menu, index) in tableAttributes">
+            <th>글 번호</th>
+            <th>글 제목</th>
+            <th>글 내용</th>
+            <th>작성자</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>교통비</td>
+            <td>45000</td>
+        </tr>
+        </tbody>
+        <tfoot>
+          <tr>
+            <td>총 합계</td>
+            <td>103000</td>
+          </tr>
+        </tfoot>
+      </table>
     </div>
-
+    <div class="footer">
+      
+    </div>
 
   </div>
-
 </template>
 
-
 <script>
+import tableAttributes from '../data_storage/tableAttributes'
+
 export default {
-  
- 
+  name: 'list',
+  data() {
+    return {
+      tablelist: tablelist
+    }
+  }
 }
 </script>
 
 <style>
+body {
+  background-color: lightgray;
+}
 
-.container {
-  /* text-align: center; */
+* {
+  font-family: sans-serif;
+}
+
+ .header{
+  background-color:purple;
+  height: 70px;
+  position: relative;
   display: flex;
-  flex-direction: column;
-}
+  align-items: center;
+ }
 
-contents {
-  margin: 10px;
-  background-color: yellow;
-}
-
-.linkBtn {
-  margin-top: 10px;
+ .header #main_title {
+  position: absolute;
+  text-decoration-style: none;
   font-size: 30px;
-  
+  color: white;
+  left: 10px;
+  background-color:black;
+}
+
+.header #sub_menu {
+  position: absolute;
+  text-decoration-style: none;
+  font-size: 20px;
+  right: 20px;
+  border: 3px solid white;
+  margin: 10px;
+  padding: 3px;
+  color: white;
+}
+
+.footer {
+  position: absolute;
+  bottom: 0px;
+  width: 100%;
+  height: 50px;
+  background-color: purple;
 }
 
 </style>
--- pages/submenu/write.vue
+++ pages/submenu/write.vue
@@ -1,26 +1,97 @@
 <template>
   <div class="container">
     <div class ="header">
-      <!-- <h1 class="title">
-        게시판
-      </h1> -->
-      <!-- <button @click="write">새 글 쓰기</button> -->
+      <NuxtLink to="/" id = "main_title">게시판</NuxtLink>
     </div>
-      <h2>글쓰기</h2>
-      <!-- <NuxtLink to="/test" class="linkBtn">테스트</NuxtLink>  -->
-      <Nuxt />
+    <div class="content">
+      <div class="write_area">
+        <h2>새 글 쓰기</h2>
+        <div class="input_content">
+          <div>닉네임</div>
+          <input v-model="nickname" type="text" />
+        </div>
+        <div class="input_content">
+          <div>제 목</div>
+          <input v-model="title" type="text" />
+        </div>
+        <div class="input_content">
+          <div>글 내용</div>
+          <div>
+            <textarea v-model="content" type="text" />
+          </div>
+        </div>
+        <div class="button_gathering">
+          <button @click ="cancle_writing" class = "cancel_action">취소</button> 
+        </div>
+      </div>
+    </div>
+      <!-- <Nuxt /> -->
+    <div class="footer">
+      
+    </div>
   </div>
 </template>
 
 
 <script>
 export default {
-  
- 
+  name: 'write',
+  data() {
+    return {
+      nickname: '',
+      title: '',
+      content: '',
+    }
+  },
+
+  methods: {
+    cancel_action(){
+        alert("글이 삭제되었습니다! 리스트로 이동합니다");
+        this.$router.push({
+                path: './..'
+            }) 
+      // this.$router.push({path:"/", query:this.body});
+    },
+
+    write(){
+
+      let contentIndex = this.$store.state.list.length + 1
+
+      let content = {
+
+        nickname : this.nickname,
+        title : this.title,
+        content : this.content,
+        contentIndex : contentIndex
+        
+      }
+
+      this.$store.dispatch("addContentsToList", content)
+      this.$router.push({
+        path:'/list'
+      })   
+    }
+  }
+
 }
 </script>
 
 <style>
+ .header{
+  background-color:purple;
+  height: 70px;
+  position: relative;
+ }
+
+ .header #main_title {
+  position: absolute;
+  text-decoration-style: none;
+  font-size: 30px;
+  color: white;
+  top: 17px;
+  left: 10px;
+  background-color:black;
+}
 
 .container {
   width: 100%;
@@ -29,16 +100,37 @@ export default {
   flex-direction: column;
 }
 
-
-
-h2 {
-  margin: 10px;
+.content {
+  width: 50%;
+  height: 100%;
+  align-items: center;
+  background-color: lightsalmon;
+  margin: 70px auto 50px;
+  padding: 5px;
+  /* border: 10px solid black; */
 }
 
-.linkBtn {
-  margin-top: 10px;
-  font-size: 30px;
-  
+.content h2 {
+  top: 0px;
+  left: 0px;
+  background-color: green;
+}
+
+.input_content{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 10px auto;
+  width: 70%;
+
+}
+
+.footer {
+  position: absolute;
+  bottom: 0px;
+  width: 100%;
+  height: 50px;
+  background-color: purple;
 }
 
 </style>
