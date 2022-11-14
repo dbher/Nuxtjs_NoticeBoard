734b344f779883611471c84965eb4c8ac4c27334
zeldabean <irenelemon00@naver.com>
2022-11-14 23:16:14 +0900

    add createPersistedState plugin and cookie and jscookie

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '@/plugins/antd-ui'
+    '@/plugins/antd-ui',
+    '@/plugins/persistedState.client.js'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
