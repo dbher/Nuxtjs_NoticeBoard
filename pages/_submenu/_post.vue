ec1f153ea4996c1bd19712df1d01b14d16030645
BEAN <irenelemon00@naver.com>
2023-02-12 19:59:23 +0900

    add 수정 기능

--- pages/_submenu/_post.vue
+++ pages/_submenu/_post.vue
@@ -1,220 +1,227 @@
 <template>
-	<div class="container">
-		<div class="header">
-			<NuxtLink to="/" id="main_title">게시판</NuxtLink>
-		</div>
-		<div class="ContentTable">
-			<div class="post_area">
-				<h2>글 제목</h2>
-				<div class="input_content">
-					<div>제 목</div>
-					<div>{{ findList.title }}</div>
-				</div>
-				<div class="input_content">
-					<div>닉네임</div>
-					<div>{{ findList.nickname }}</div>
-				</div>
-				<div class="input_content">
-					<div>글 내용</div>
-					<div>{{ findList.content }}</div>
-				</div>
-				<div class="button_gathering">
-					<button @click="updatePost" class="revicePost">수정</button>
-					<button @click="deletePost" class="deletePost">삭제</button>
-				</div>
-			</div>
-		</div>
-		<div class="comment_area">
-			<div class="add_comment">
-				<h3>댓글창</h3>
-				<div class="input_comment_area">
-					<span>닉네임</span>
-					<input placeholder="닉네임" v-model="commentNickname" type="text" />
-					<span>댓글</span>
-					<input placeholder="댓글을 입력하세요" v-model="commentContent" type="text" />
-				<button @click="addComment">댓글달기</button>
-				</div>
-				<div class="commentTable">
-					<div :key="index" v-for="(user, index) in findCommentData" class="comment_box">
-						<div>
-							<span>{{user.commentNickname}}</span>
-							<span>/</span>
-							<span>{{user.commentContent}}</span>
-							<span @click="deleteComment">[삭제]</span>
-							<span>[수정]</span>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<!-- <Nuxt /> -->
-		<div class="footer">
-		</div>
-	</div>
+  <div class="container">
+    <div class="header">
+      <NuxtLink to="/" id="main_title">게시판</NuxtLink>
+    </div>
+    <div class="ContentTable">
+      <div class="post_area">
+        <h2>글 제목</h2>
+        <div class="input_content">
+          <div>제 목</div>
+          <div>{{ findList.title }}</div>
+        </div>
+        <div class="input_content">
+          <div>닉네임</div>
+          <div>{{ findList.nickname }}</div>
+        </div>
+        <div class="input_content">
+          <div>글 내용</div>
+          <div>{{ findList.content }}</div>
+        </div>
+        <div class="button_gathering">
+          <button @click="updatePost" class="revicePost">수정</button>
+          <button @click="deletePost" class="deletePost">삭제</button>
+        </div>
+      </div>
+    </div>
+    <div class="comment_area">
+      <div class="add_comment">
+        <h3>댓글창</h3>
+        <div class="input_comment_area">
+          <span>닉네임</span>
+          <input placeholder="닉네임" v-model="commentNickname" type="text" />
+          <span>댓글</span>
+          <input
+            placeholder="댓글을 입력하세요"
+            v-model="commentContent"
+            type="text"
+          />
+          <button @click="addComment">댓글달기</button>
+        </div>
+        <div class="commentTable">
+          <div
+            :key="index"
+            v-for="(user, index) in findCommentData"
+            class="comment_box"
+          >
+            <div>
+              <span>{{ user.commentNickname }}</span>
+              <span>/</span>
+              <span>{{ user.commentContent }}</span>
+              <span @click="deleteComment">[삭제]</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- <Nuxt /> -->
+    <div class="footer"></div>
+  </div>
 </template>
 
 
 <script>
-
 export default {
-	ssr : "false",
-	name: "list",
-	data() {
-		return {
-			commentID: '',
-			commentContent: '',
-			commentList: ''
-			// Tablelist: tableAttributes
-		};
-	},
+  ssr: "false",
+  name: "list",
+  data() {
+    return {
+      commentID: "",
+      commentNickname: "",
+      commentContent: "",
+      commentList: this.$store.state.commentList,
+      // Tablelist: tableAttributes
+    };
+  },
 
-	computed: {
+  computed: {
+    index() {
+      return this.$route.params.post;
+    },
 
-		index() {
-			return this.$route.params.post;
-		},
+    callData() {
+      return this.$store.state.list;
+    },
 
-		callData() {
-			return this.$store.state.list;
-		},
+    findList() {
+      return this.callData[this.index];
+    },
 
-		findList() {
-			return this.callData[ this.index ];
-		},
+    findCommentData() {
+      return this.$store.state.commentList.filter((comment) => {
+        return comment.contentId === this.contentIndex;
+      });
+    },
+  },
 
-		findCommentData () {
-			return this.$store.state.commentList.filter(comment => {
-				return comment.contentId === this.contentIndex;
-			})
-		}
-	},
+  methods: {
+    checkContent() {
+      console.log(contentIndex);
+    },
 
-	methods: {
-		checkContent() {
-			console.log( contentIndex );
-		},
+    deletePost() {
+      const deleteIndex = this.findList.contentIndex;
 
-		deletePost() {
-			const deleteIndex = this.findList.contentIndex;
+      this.$store.dispatch("deleteContentsToList", deleteIndex);
+      alert("글 삭제가 완료되었습니다. 게시판으로 이동합니다");
+      this.$router.push("/");
+    },
 
-			this.$store.dispatch( "deleteContentsToList", deleteIndex );
-			alert( "글 삭제가 완료되었습니다. 게시판으로 이동합니다" );
-			this.$router.push( "/" );
-		},
+    updatePost() {
+      const params = {
+        nickname: this.findList.nickname,
+        title: this.findList.title,
+        content: this.findList.content,
+        contentIndex: this.findList.contentIndex,
+      };
 
-		updatePost() {
+      console.log(params.contentIndex);
+	  
+      this.$router.push({
+        path: "/_submenu/update",
+        query: { targetList: params },
+      });
+    },
 
-			const params = {
-				nickname: this.findList.nickname,
-				title   : this.findList.title,
-				content : this.findList.content,
-				contentIndex : this.findList.contentIndex
-			};
-			console.log(params.contentIndex);
-			this.$router.push( {
-				path : "/_submenu/update",
-				query: { targetList: params },
-			} );
-		},
+    addComment() {
+      let commentIndex = this.commentList.length;
 
-		addComment() {
-			let commentIndex = this.commentList.length + 1;
+      console.log("commentIndex");
+      console.log(commentIndex);
+      let commentData = {
+        commentContentId: this.findList.contentIndex,
+        commentIndex: commentIndex,
+        commentNickname: this.commentNickname,
+        commentContent: this.commentContent,
+      };
+      console.log(commentData);
+      this.$store.dispatch("addCommentToList", commentData);
+    },
 
-            let commentData = {
-                commentContentId: this.findList.contentIndex,
-                commentNickname: this.commentNickname,
-                commentContent: this.commentContent
-            }
-			console.log("contentIndex");
-			console.log(commentData.commentContentId);
-			this.$store.dispatch("addCommentToList", commentData);
-		},
+    deleteComment() {
+      const deleteIndex = this.contentIndex;
+      console.log("삭제 인덱스");
+      // console.log(deleteIndex);
+      console.log(this);
 
-		deleteComment() {
-			const deleteIndex = this.findCommentData.commentContentId;
-
-			this.$store.dispatch("deleteCommentToList", deleteIndex);
-		}
-	},
-
+      this.$store.dispatch("deleteCommentToList", deleteIndex);
+    },
+  },
 };
 </script>
 
 <style>
 .header {
-	background-color: #e1bee7;
-	height: 70px;
-	position: relative;
+  background-color: #e1bee7;
+  height: 70px;
+  position: relative;
 }
 
 .header #main_title {
-	position: absolute;
-	text-decoration-style: none;
-	font-size: 30px;
-	color: white;
-	top: 17px;
-	left: 10px;
+  position: absolute;
+  text-decoration-style: none;
+  font-size: 30px;
+  color: white;
+  top: 17px;
+  left: 10px;
 }
 
 .container {
-	width: 100%;
-	/* text-align: center; */
-	display: flex;
-	flex-direction: column;
+  width: 100%;
+  /* text-align: center; */
+  display: flex;
+  flex-direction: column;
 }
 
 .ContentTable {
-	width: 90%;
-	height: 100%;
-	align-items: center;
-	background-color: #fff1ff;
-	margin: 70px auto 50px;
-	padding: 5px;
-	/* border: 10px solid black; */
+  width: 90%;
+  height: 100%;
+  align-items: center;
+  background-color: #fff1ff;
+  margin: 70px auto 50px;
+  padding: 5px;
+  /* border: 10px solid black; */
 }
 
 .comment_area {
-	width: 90%;
-	height: 100%;
-	background: lemonchiffon;
-	align-items: center;
-	margin: 10px auto 50px;
-	padding: 5px;
-
+  width: 90%;
+  height: 100%;
+  background: lemonchiffon;
+  align-items: center;
+  margin: 10px auto 50px;
+  padding: 5px;
 }
 
-.comment_area button{
-	/* background: red;  */
-	align-items: right;
-	text-align: right;
+.comment_area button {
+  /* background: red;  */
+  align-items: right;
+  text-align: right;
 }
 
 .content h2 {
-	top: 0px;
-	left: 0px;
-	background-color: green;
+  top: 0px;
+  left: 0px;
+  background-color: green;
 }
 
 .input_content {
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
-	margin: 10px auto;
-	width: 70%;
-
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 10px auto;
+  width: 70%;
 }
 
 .button_gathering {
-	margin-top: 20px;
-	text-align: center;
+  margin-top: 20px;
+  text-align: center;
 }
 
 .footer {
-	position: absolute;
-	bottom: 0px;
-	width: 100%;
-	height: 50px;
-	background-color: #e1bee7;
+  position: absolute;
+  bottom: 0px;
+  width: 100%;
+  height: 50px;
+  background-color: #e1bee7;
 }
-
 </style>
--- store/index.js
+++ store/index.js
@@ -4,56 +4,53 @@ export const state = () => ({
 });
 
 export const mutations = {
-	addContent(state, list){
+	addContent(state, list) {
 		state.list.push(list)
 	},
 
-	deleteContent(state, index){
+	deleteContent(state, index) {
 		state.list.splice(index, 1);
 	},
 
-	updateContent(state, index, content){
-		state.list.splice(index, 1, content);
+	updateContent(state, editedPost) {
+		state.list.splice(editedPost.contentIndex - 1, 1, editedPost);
 	},
 
-	addComment(state, comment){
+	addComment(state, comment) {
 		state.commentList.push(comment);
 	},
 
-	deleteComment(state, index){
+	deleteComment(state, index) {
 		state.commentList.splice(index, 1);
 	}
 }
 
 export const actions = {
-	addContentsToList({commit}, list){
+	addContentsToList({ commit }, list) {
 		commit('addContent', list)
-			console.log(list);
+		console.log(list);
 	},
 
-	deleteContentsToList({commit}, index){
+	deleteContentsToList({ commit }, index) {
 		commit('deleteContent', index)
-			console.log(index);
-	},
-
-	updateContentsToList({commit}, index, content){
 		console.log(index);
-		console.log(content); //content undefined 이슈 있음
-		if (content != undefined) //박살방지용
-			commit('updateContent', index, content);
 	},
 
-	addCommentToList({commit}, list){
+	updateContentsToList({ commit }, editedPost) {
+		commit('updateContent', editedPost);
+	},
+
+	addCommentToList({ commit }, list) {
 		commit('addComment', list)
 	},
 
-	deleteCommentToList({commit}, index){
+	deleteCommentToList({ commit }, index) {
 		commit('deleteComment', index);
 	}
 }
 
 export const getters = {
-	getList(state){
+	getList(state) {
 		return state.list;
 	},
 	// reverseList: state => {
